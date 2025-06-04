25b019d0fdbd3d5fd176f5546f1519d5d3d3b885
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2025-06-04 15:48:10 -0600

    Se creó un ejemplo muy básico para la pantalla de volumen, también se eliminó el código que viene por defecto en el main y solo hacer la llamada a las pantallas.

--- antunez_app/lib/main.dart
+++ antunez_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:antunez_app/screens/screen_volume.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,45 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ScreenVolume()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Hola:'),
-
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-               
-              },
-              child: const Text('Iniciar'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
--- antunez_app/lib/screens/screen_volume.dart
+++ antunez_app/lib/screens/screen_volume.dart
@@ -1,2 +1,26 @@
 // Kevin Rafael Díaz López - 04/06/2025
 // Archivo que contiene la pantalla de calcular volumen
+import 'package:flutter/material.dart';
+class ScreenVolume extends StatelessWidget {
+  const ScreenVolume({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calcular Volumen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Aquí va el contenido para calcular volumen',
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
