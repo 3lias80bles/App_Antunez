1dd632ac432d779e2e0e1ec07180ef1b039f0158
Rosendo Eden Mendoza Casarrubia <156049426+EdenMCa@users.noreply.github.com>
2025-06-06 11:27:36 -0600

    Se agregaron las rutas de las pantallas a main.dart

--- antunez_app/lib/main.dart
+++ antunez_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:antunez_app/screens/screen_volume.dart';
 import 'package:antunez_app/screens/screen_biomas.dart';
 import 'package:antunez_app/configurations/app_theme.dart';
+import 'package:antunez_app/screens/home_screen.dart';
+import 'package:antunez_app/screens/screen_volume.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aplicacion forestal',
       theme: appTheme,
       debugShowCheckedModeBanner: false,
-      home: const ScreenBiomas()
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (_) => const HomeScreen(),
+        ScreenBiomas.routeName: (_) => const ScreenBiomas(),
+        ScreenVolume.routeName: (_) => const ScreenVolume(),
+      },
     );
   }
 }
